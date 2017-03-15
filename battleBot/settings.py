27d3c735319ce0dfda22b1f7897d09f05ada74e4
IxLahiji <ixl164@usask.ca>
2017-03-15 15:08:46 -0600

    Separated settings from main bot

---
+++ battleBot/settings.py
@@ -0,0 +1,58 @@
+import json
+import os.path
+import sys
+import time
+
+class JSONSettings:
+
+	settings_path = ""
+	parsed_settings = {}
+
+	def __init__(self, program_path):
+		self.settings_path = program_path + os.path.sep + 'settings.JSON'
+		self.settings_chk()
+		self.read_settings()
+
+		
+	def write_settings(self, settings):
+		#Open JSON credentials file for writing
+		settings_file = open(self.settings_path, 'w')
+		json.dump(settings, settings_file, indent=4, sort_keys=True)
+		settings_file.close()
+
+
+	def read_settings(self):
+		#Open JSON credentials file for reading and parse
+		settings_file = open(self.settings_path, 'r')
+		self.parsed_settings = json.loads(settings_file.read())
+		settings_file.close()
+
+
+	def settings_chk(self):
+		#Generate credential and blacklist JSON files on startup if not present
+		if not os.path.exists(self.settings_path):
+			print("No settings file found. Generating a new settings JSON file...")
+			#Defining keys for the JSON file, as well as configuring default values
+			default_json = """{"discord_token": ""}
+					   """
+			parsed_json = json.loads(default_json)
+			self.write_settings(parsed_json)
+			
+			print("The settings file has been generated. Please add a token for your discord bot before the next execution.")
+			time.sleep(3)
+			sys.exit()
+
+			
+	def get_token(self):
+		#Check to make sure token is present
+		if (not ('discord_token' in self.parsed_stats)):
+			print ("Invalid 'settings.JSON' file. Please delete the file, and restart application.")
+			time.sleep(3)
+			sys.exit()
+		print (self.parsed_settings)
+		if (not self.parsed_settings['discord_token']):
+			print ("Please enter a discord bot token in 'settings.JSON' before running")
+			time.sleep(3)
+			sys.exit()
+		else:
+			return self.parsed_settings['discord_token']
--- lahujBattleBot.py
+++ lahujBattleBot.py
@@ -6,6 +6,8 @@ import json
 import os.path
 import sys
 import time
+from battleBot.settings import JSONSettings
+from battleBot.stats import JSONStats
 
 
 SUCCESS = 0
@@ -15,15 +17,15 @@ NOT_COMMAND = 3
 INVALID_ARGUMENTS = 4
 
 
-#Program path
 prog_path = os.path.dirname(os.path.abspath(__file__))
-settings_path = prog_path + os.path.sep + 'settings.JSON'
+
+#Load information
+settings = JSONSettings(prog_path)
+stats = JSONStats(prog_path)
 
 #Create new discord client
 client = discord.Client()
 
-#Define the settings
-parsed_settings = []
 
 #============[KeyboardInterrupt Handling]============
 
@@ -40,48 +42,7 @@ async def wakeup():
 client.loop.create_task(wakeup())
 
 
-#============[Helper Functions]============
-
-async def write_settings(settings):
-    settings_file_w = open(settings_path, 'w')
-    json.dump(settings, settings_file_w, indent=4, sort_keys=True)
-    settings_file_w.close()
-
-
 #============[Executed at Startup]============
-
-#Generate credential and blacklist JSON files on startup
-if not os.path.exists(settings_path):
-    print("No settings file found. Generating a new settings JSON file...")
-    new_settings_file = open(settings_path, 'w')
-
-    #Defining keys for the JSON file, as well as configuring default values
-    default_json = """{"discord_token": ""}
-               """
-    parsed_json = json.loads(default_json)
-    json.dump(parsed_json, new_settings_file, indent=4, sort_keys=True)
-    new_settings_file.close()
-    
-    time.sleep(1)
-    print("The settings file has been generated. Please add a token for your discord bot before the next execution.")
-    time.sleep(3)
-    sys.exit()
-
-
-#Open JSON credentials file for reading and parse
-settings_file = open(settings_path, 'r')
-parsed_settings = json.loads(settings_file.read())
-settings_file.close()
-
-#Re-writing to fix formatting issues that may be present after the user edits json file
-asyncio.ensure_future(write_settings(parsed_settings))
-
-
-#Check to make sure token is present
-if (not parsed_settings['discord_token']):
-    print ("Please enter a discord bot token in 'settings.JSON' before running")
-    sys.exit()
-
 
 #on_ready event, typically on login
 @client.event
@@ -92,7 +53,7 @@ async def on_ready():
 
 #Run client (connect and login) ~ Blocking (must be last) ~ This is an unabstracted version of client.run() to give more control
 try:
-    client.loop.run_until_complete(client.start(parsed_settings['discord_token']))
+    client.loop.run_until_complete(client.start(settings.get_token()))
     
 except KeyboardInterrupt:
     #Set exit flag to allow wakeup() to close properly
